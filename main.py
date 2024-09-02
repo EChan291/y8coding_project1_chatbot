255f360d62350feef1e9225ac34108a54d6308f5
EChan291 <EChan29@kellettschool.com>
2024-09-02 06:59:55 +0000

    coding

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # your chatbot code here!
+
+ = ("Enter your name! ")
+print(name, "it's you!", name, "i
+      
+siblings = int(input(("How many brothers and sisters do you have?")
+
+if  == 
+print("You're an only child.")
