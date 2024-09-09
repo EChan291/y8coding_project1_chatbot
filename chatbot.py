df7ca80fe9ad9d518836275678e5bfaa82278edb
EChan291 <EChan29@kellettschool.com>
2024-09-09 07:12:43 +0000

    chatbot

---
+++ chatbot.py
@@ -0,0 +1,7 @@
+name =input("What is your name?")
+print("Hello", {name}"!")
+age = int(input("How old are you?"))
+if age <= 10:
+    print("{age} is a good age to be!")
+elif age <=18:
+    print("{age}")
---
+++ lesson3.py
@@ -0,0 +1,6 @@
+apple ="pinapple"
+chicken = len(apple)
+fish = chicken + chicken
+frog = fish * apple
+print(frog+frog)
+
